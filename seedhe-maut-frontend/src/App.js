f5512960584916f5de22a4bfe543463e71f8470b
Vishal-sharmak7 <vishalsharmak7@gmail.com>
2025-02-24 15:59:56 +0530

    signup - login implementation

--- seedhe-maut-frontend/src/App.js
+++ seedhe-maut-frontend/src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
-import Login from "./pages/login/Login"; // Import Login component
-import "./pages/login/Login"; // Import CSS for styling
-import Signup from "./pages/signup/Signup"; 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <div>
+      {/* Toast notifications */}
+      <ToastContainer 
+  position="top-right" 
+  autoClose={5000}  /* Set it to 5 seconds */
+  hideProgressBar={false} 
+  newestOnTop={false} 
+  closeOnClick 
+  rtl={false} 
+  pauseOnFocusLoss 
+  draggable 
+  pauseOnHover 
+  theme="colored" 
+/>
+
+
+
+      {/* App Routes */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
   );
 }
 
 export default App;
-
