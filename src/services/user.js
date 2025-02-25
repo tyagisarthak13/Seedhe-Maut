5d77374c7bc7652e2dc969ce9657df4d1daabb76
tyagisarthak13 <sarthaktyagi797@gmail.com>
2025-02-25 14:26:14 +0530

    logout api

--- src/services/user.js
+++ src/services/user.js
@@ -2,8 +2,9 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.js";
+import { sendMail } from "../utils/commonfunction.js";
 
-// Signup Route
+//! Signup Route
 export const signup = async (req, res) => {
   try {
     console.log(req.body);
@@ -39,7 +40,7 @@ export const signup = async (req, res) => {
   }
 };
 
-// Login Route
+//! Login Route -
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -82,9 +83,54 @@ export const login = async (req, res) => {
   }
 };
 
+//! User Details by ID -
 export const userDetails = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const userDetails = req.user;
+    console.log(userDetails);
+    const userData = await User.findById({ _id: userDetails._id }).lean();
+    if (!userDetails) res.status(404).send("User not found!!");
+    return res
+      .status(200)
+      .json({ msg: "User found", status: true, data: userData });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message, status: false });
+  }
+};
+
+//! Forget Password
+export const forgetPassword = async (req, res) => {
+  try {
+    const generateToken = jwt.sign(
+      {
+        _id: req.user._id,
+        email: req.user.email,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      }
+    );
+
+    const emailData = await User.findOne({ email: req.user.email }).lean();
+    if (!emailData) res.status(400).send("Email not found!!");
+    const link = `${process.env.BASE_URL}/v1/user/forgetPassword?accessToken=${generateToken}`;
+    console.log(link);
+    await sendMail(req.user.email);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message, status: false });
+  }
+};
+
+//! Logout
+export const userLogout = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(decoded._id, {
+      $set: {
+        refreshToken: null,
+      },
+    });
+    return res.status(200).json("user logged out successfully!");
   } catch (error) {
     return res.status(500).json({ msg: error.message, status: false });
   }
